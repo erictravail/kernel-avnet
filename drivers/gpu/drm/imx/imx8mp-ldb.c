ec35ad80d04e9f1aceb954ab3bc7a7a5f61c70ca
Waldemar Glensk <waldemar.glensk@avnet.com>
2021-10-20 14:15:05 +0200

    drivers/gpu/drm/imx/imx8mp-ldb. Remove LVDS/pixel clock limitations. (MLDK-3077) (IMU-467)

--- drivers/gpu/drm/imx/imx8mp-ldb.c
+++ drivers/gpu/drm/imx/imx8mp-ldb.c
@@ -160,9 +160,7 @@ imx8mp_ldb_encoder_atomic_check(struct drm_encoder *encoder,
 	struct imx8mp_ldb_channel *imx8mp_ldb_ch =
 						enc_to_imx8mp_ldb_ch(encoder);
 	struct ldb_channel *ldb_ch = &imx8mp_ldb_ch->base;
-	struct imx8mp_ldb *imx8mp_ldb = imx8mp_ldb_ch->imx8mp_ldb;
-	struct ldb *ldb = &imx8mp_ldb->base;
-	struct drm_display_mode *mode = &crtc_state->adjusted_mode;
+
 	struct drm_bridge_state *bridge_state = NULL;
 	struct drm_bridge *bridge;
 
@@ -186,15 +184,6 @@ imx8mp_ldb_encoder_atomic_check(struct drm_encoder *encoder,
 		return -EINVAL;
 	}
 
-	/*
-	 * Due to limited video PLL frequency points on i.MX8mp,
-	 * we do mode fixup here in case any mode is unsupported.
-	 */
-	if (ldb->dual)
-		mode->clock = mode->clock > 100000 ? 148500 : 74250;
-	else
-		mode->clock = 74250;
-
 	return 0;
 }
 
@@ -205,22 +194,10 @@ imx8mp_ldb_encoder_mode_valid(struct drm_encoder *encoder,
 	struct imx8mp_ldb_channel *imx8mp_ldb_ch =
 						enc_to_imx8mp_ldb_ch(encoder);
 	struct ldb_channel *ldb_ch = &imx8mp_ldb_ch->base;
-	struct imx8mp_ldb *imx8mp_ldb = imx8mp_ldb_ch->imx8mp_ldb;
-	struct ldb *ldb = &imx8mp_ldb->base;
 
 	/* it should be okay with a panel */
 	if (ldb_ch->panel)
 		return MODE_OK;
-
-	/*
-	 * Due to limited video PLL frequency points on i.MX8mp,
-	 * we do mode valid check here.
-	 */
-	if (ldb->dual && mode->clock != 74250 && mode->clock != 148500)
-		return MODE_NOCLOCK;
-
-	if (!ldb->dual && mode->clock != 74250)
-		return MODE_NOCLOCK;
 
 	return MODE_OK;
 }
